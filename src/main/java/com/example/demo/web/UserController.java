62f19d6b2b06a617fe83cd9f7848128a4bcdc5f7
adachiryohei <ryohei.adachi@rakus.co.jp>
2017-06-14 15:57:07 +0900

    清水株式会社掲示板作成。

---
+++ src/main/java/com/example/demo/web/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.domain.User;
+
+@Controller
+
+public class UserController {
+	private List<User> userList=new ArrayList<User>();
+	
+	@RequestMapping("/")
+	public String index(){
+		return "bbs";
+	}
+	
+	@RequestMapping("/create")
+	public String create(UserForm form,Model model){
+		String name=form.getName();
+		String message=form.getMessage();
+		
+		User user=new User();
+		user.setName(name);
+		user.setMessage(message);
+		userList.add(user);
+		
+		model.addAttribute("userList", userList);
+		return "bbs";
+	}
+	
+	@ModelAttribute
+	public UserForm setUp(){
+		return new UserForm();
+	}
+	
+}
